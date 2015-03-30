d07a228efb34e792b0d22a55b9a1f7e6d6c6ce9b
sisve <sinsvend@gmail.com>
2015-03-31 00:37:35 +0200

    Fixed checkstyle by upgrading to a new version

--- gulp/tasks/check-style.js
+++ gulp/tasks/check-style.js
@@ -2,5 +2,4 @@ var gulp = require('gulp');
 var styleCheck = require('sol-style-check');
 
 gulp.task('style-check', styleCheck(
-  ['!coverage{,/**}', '!public/{,/**}'],
-  'node_modules/sol-style-check/lib/jscs/react.jscsrc'));
+  ['!coverage{,/**}', '!public/{,/**}'], 'esprima-fb'));
