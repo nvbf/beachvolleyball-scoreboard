1a23d0bf3d3e15a682e5c82a33679ef84ee725ea
sisve <sinsvend@gmail.com>
2015-03-30 02:02:07 +0200

    excluded generated files from check-style

--- gulp/tasks/check-style.js
+++ gulp/tasks/check-style.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
 var styleCheck = require('sol-style-check');
 
-gulp.task('style-check', styleCheck('', 'node_modules/sol-style-check/lib/jscs/react.jscsrc'));
+gulp.task('style-check', styleCheck(
+  ['!coverage{,/**}', '!public/{,/**}'],
+  'node_modules/sol-style-check/lib/jscs/react.jscsrc'));
