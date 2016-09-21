f07a11f8acc1d4c0840776a989016c7928c06f9a
Sindre Svendby <sinsvend@gmail.com>
2016-09-21 22:10:39 +0200

    added webpack with hmr enabled

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require("webpack");
+var path = require("path");
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', "stage-0"]
+        }
+      }
+    ]
+  },
+  entry: {
+    app: ["./src/js/Router.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "public/dynamic"),
+    publicPath: "dynamic",
+    filename: "app.js"
+  },
+  devServer: {
+    inline: true,
+    hot: true,
+    contentBase: "public/"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+};
