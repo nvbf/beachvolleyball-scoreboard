51c49eb9e887a6a6aa6ea2bc7fb6845b07fe5af5
Sindre Svendby <sinsvend@gmail.com>
2017-06-08 16:50:35 +0200

    Added clock, ticking

--- src/components/detail-toggle.js
+++ src/components/detail-toggle.js
@@ -1,41 +1,40 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 class DetailsAndClock extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {date: new Date()}
-	}
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
 
-	componentDidMount() {
-		// this.interval = setInterval(this.updateClock, 1000)
-	}
+  componentDidMount() {
+    this.interval = setInterval(this.updateClock, 1000);
+  }
 
-	updateClock = () => {
-		this.setState({date: new Date()})
-	}
+  updateClock = () => {
+    this.setState({ date: new Date() });
+  };
 
-	componentWillUnmount() {
-		console.log('unmount')
-		clearInterval(this.interval)
-	}
+  componentWillUnmount() {
+    console.log("unmount");
+    clearInterval(this.interval);
+  }
 
-	render() {
-		const {	date } = this.state;
-		return (
-			<label>
-				<span> Details {formatTime(date)}</span>
-			</label>
-		);
-	}	
-};
-
+  render() {
+    const { date } = this.state;
+    return (
+      <label>
+        <span> Details {formatTime(date)}</span>
+      </label>
+    );
+  }
+}
 
 function formatTime(date) {
-	const hour = date.getHours();
-	const min = date.getMinutes();
-	const sec = date.getSeconds();
-	return `${hour}:${min}:${sec}`;
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  return `${hour}:${min}:${sec}`;
 }
 
 export default DetailsAndClock;
