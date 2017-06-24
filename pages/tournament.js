5b59a1d06e440ea49b1534143efd3fc294e83354
Sindre Svendby <sinsvend@gmail.com>
2017-06-24 02:15:09 +0200

    update tournaments

--- pages/tournament.js
+++ pages/tournament.js
@@ -7,7 +7,11 @@ import { transformToCorrectState } from "../src/domain/tide/storage";
 // import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 // import { Card, CardActions, CardHeader, CardText } from "material-ui";
 
-import { constants as c, Match } from "../src/domain/tide/state";
+import {
+  constants as c,
+  Match,
+  MATCH_FIREBASE_KEY
+} from "../src/domain/tide/state";
 
 import {
   isMatchFinished,
@@ -81,43 +85,13 @@ function showScore(match = {}) {
   const pointsInCurrentSet = getPointsInCurrentSetAsString(match);
   const scoreInCompletedSet = getScoreForCompletedSets(match);
 
-  const endGameSets = {
-    fontSize: "2.5em",
-    color: "#333",
-    textAlign: "center"
-  };
+  return <span>{firstSet}, {secondSet}, {thirdSet}</span>;
+}
 
-  const currentPlayedSetsScore = {
-    textAlign: "center"
-  };
-
-  if (isFinished) {
-    return (
-      <div>
-        <div style={endGameSets}>
-          {setResult}
-        </div>
-        <div style={currentPlayedSetsScore}>
-          {firstSet}, {secondSet}, {thirdSet}
-        </div>
-      </div>
-    );
-  }
-
-  const currentScoreStyle = {
-    fontSize: "2.5em",
-    color: "red",
-    textAlign: "center"
-  };
-
-  return (
-    <div>
-      <div style={currentScoreStyle}>
-        {pointsInCurrentSet}
-      </div>
-      <div style={currentPlayedSetsScore}>{scoreInCompletedSet}</div>
-    </div>
-  );
+function compare(a, b) {
+  if (a.matchId < b.matchId) return -1;
+  if (a.matchId > b.matchId) return 1;
+  return 0;
 }
 
 function listMatches(matches) {
@@ -125,69 +99,67 @@ function listMatches(matches) {
     return;
   }
   console.log(matches);
-  return Object.keys(matches).map(matchKey => {
-    console.log("matchKey", matchKey);
-    console.log("matches[matchKey]", matches[matchKey]);
-    const asJson = JSON.parse(matches[matchKey].match);
-    console.log("asJson", asJson);
-    const state = transformToCorrectState(asJson);
-    console.log("state", state);
-    const match = state[c.MATCH];
-    console.log("match", match);
+  return Object.keys(matches)
+    .map(matchKey => {
+      console.log("matchKey", matchKey);
+      console.log("matches[matchKey]", matches[matchKey]);
+      const asJson = JSON.parse(matches[matchKey].match);
+      console.log("asJson", asJson);
+      const state = transformToCorrectState(asJson);
+      console.log("state", state);
+      const match = state[c.MATCH];
+      console.log("match", match);
 
-    const h1Player = match[c.HOMETEAM_FIRST_PLAYER_NAME];
-    const h2Player = match[c.HOMETEAM_SECOND_PLAYER_NAME];
-    const b1Player = match[c.AWAYTEAM_FIRST_PLAYER_NAME];
-    const b2Player = match[c.AWAYTEAM_SECOND_PLAYER_NAME];
+      const h1Player = match[c.HOMETEAM_FIRST_PLAYER_NAME];
+      const h2Player = match[c.HOMETEAM_SECOND_PLAYER_NAME];
+      const b1Player = match[c.AWAYTEAM_FIRST_PLAYER_NAME];
+      const b2Player = match[c.AWAYTEAM_SECOND_PLAYER_NAME];
 
-    const h1Points = match[c.FIRST_SET][c.HOMETEAM_POINT];
-    const b1Points = match[c.FIRST_SET][c.AWAYTEAM_POINT];
-    const h2Points = match[c.SECOND_SET][c.HOMETEAM_POINT];
-    const b2Points = match[c.SECOND_SET][c.AWAYTEAM_POINT];
-    const h3Points = match[c.THIRD_SET][c.HOMETEAM_POINT];
-    const b3Points = match[c.THIRD_SET][c.AWAYTEAM_POINT];
+      const h1Points = match[c.FIRST_SET][c.HOMETEAM_POINT];
+      const b1Points = match[c.FIRST_SET][c.AWAYTEAM_POINT];
+      const h2Points = match[c.SECOND_SET][c.HOMETEAM_POINT];
+      const b2Points = match[c.SECOND_SET][c.AWAYTEAM_POINT];
+      const h3Points = match[c.THIRD_SET][c.HOMETEAM_POINT];
+      const b3Points = match[c.THIRD_SET][c.AWAYTEAM_POINT];
 
-    const matchId = match[c.MATCH_ID];
+      const matchId = match[c.MATCH_ID];
 
-    const cardTextStyle = {
-      display: "flex",
-      justifyContent: "space-between",
-      flexDirection: "row",
-      color: "#333"
-    };
+      const cardTextStyle = {
+        display: "flex",
+        justifyContent: "space-between",
+        flexDirection: "row",
+        color: "#333"
+      };
 
-    const homeContainer = {
-      fontWeight: "bold",
-      fontSize: "1.5em",
-      flexGrow: "1"
-    };
+      const homeContainer = {
+        fontWeight: "bold",
+        fontSize: "1.5em",
+        flexGrow: "1"
+      };
 
-    const scoreStyle = {
-      flexGrow: "2"
-    };
-
-    return (
-      <div>
-        {/*<Card key={matchKey}>
-          <CardText style={cardTextStyle}>*/}
-        <div style={homeContainer}>
-          <div>{h1Player}</div>
-          <div>{h2Player}</div>
+      const scoreStyle = {
+        flexGrow: "2"
+      };
+      return {
+        matchId,
+        h1Player,
+        h2Player,
+        b1Player,
+        b2Player,
+        match,
+        matchKey
+      };
+    })
+    .sort(compare)
+    .map(match => {
+      return (
+        <div>
+          {match.matchId}: {match.h1Player} / {match.h2Player} -{" "}
+          {match.b1Player} / {match.b2Player}: {" "}
+          {showScore(match.match)} ({match.matchKey})
         </div>
-        <div style={scoreStyle}>
-          {showScore(match)}
-
-        </div>
-        <div style={homeContainer}>
-          <div>{b1Player}</div>
-          <div>{b2Player}</div>
-        </div>
-        <div>matchid: {matchId}</div>
-        {/*</CardText>
-        </Card>*/}
-      </div>
-    );
-  });
+      );
+    });
 }
 
 export default Tournament;
