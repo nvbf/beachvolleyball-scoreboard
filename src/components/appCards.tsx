75c8227cbf5f2fcd10c1ded097e0ce04c11f6dd6
Øystein W. Grændsen <oystein.w.graendsen@remarkable.no>
2023-04-03 22:00:40 +0200

    refactor(vite): start using vite

--- src/components/appCards.tsx
+++ src/components/appCards.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import cardImage from '../static/img/match.jpg'
+
 import Link from '@mui/material/Link';
 import {
     Card,
@@ -17,6 +17,10 @@ interface AppCardsProps {
     path: string
 }
 
+const IMAGES = {
+    cardImage : new URL('../static/img/match.jpg', import.meta.url).href
+}
+
 export function AppCards({ title, subHeader, path }: AppCardsProps) {
     return (
         <Grid item>
@@ -24,7 +28,7 @@ export function AppCards({ title, subHeader, path }: AppCardsProps) {
                 <CardActionArea href={path}>
                     <CardMedia
                         component="img"
-                        src={cardImage}
+                        src={IMAGES.cardImage}
                         alt="Beacvolleyballcourt"
                     />
                     <CardHeader
--- src/pages/Tournements.tsx
+++ src/pages/Tournements.tsx
@@ -1,16 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
-import cardImage from './static/img/match.jpg'
-import ThemeProvider from "@mui/material/styles/ThemeProvider";
 // import RaisedButton from "@mui/material/RaisedButton";
-import {
-  Card,
-  CardHeader,
-  CardMedia,
-  CardActionArea,
-} from "@mui/material";
-import AppBarMain from "../components/appbar";
-import AppCards from "../components/appCards"
 
 function Tournements() {
   return (
