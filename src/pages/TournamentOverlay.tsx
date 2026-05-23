9518b4274823dd9e7416255ca02527ebb2b5e8ed
Øystein W. Grændsen <oystein@graendsen.no>
2026-05-23 11:14:12 +0200

    fix: set back to correct height

--- src/pages/TournamentOverlay.tsx
+++ src/pages/TournamentOverlay.tsx
@@ -251,7 +251,7 @@ const TournamentOverlay = () => {
         right: "0",
         backgroundColor: 'rgba(0, 0, 0, 0)',        // backgroundColor: "rgba(0,0,0,0.7)",
         textAlign: "center",
-        width: '1920px', height: '900px'
+        width: '1920px', height: '1080px'
       }}
     >
       {currentMatch && <Grid
